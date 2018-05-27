f8651c5d8aaf8d05dbf1f199eb5e8848be4d8462
Brian Crowell <brian@fluggo.com>
2018-05-26 22:57:02 -0500

    Declare request types for user APIs

--- server/users/wsapi.ts
+++ server/users/wsapi.ts
@@ -6,14 +6,30 @@ function canEditUsers(user: users.User): boolean {
   return !!user.getSettings().userControls.editUsers;
 }
 
-wsapi.on('user/set-user-settings', function(request, callback, notifyCallback) {
+interface SetUserSettingsRequest {
+  userSettings: users.Settings['userSettings'];
+}
+
+interface UserRequest {
+  upn: string;
+}
+
+interface SetUserControlsRequest extends UserRequest {
+  userControls: users.Settings['userControls'];
+}
+
+interface CreateUserRequest extends UserRequest {
+  settings: users.Settings;
+}
+
+wsapi.on<SetUserSettingsRequest>('user/set-user-settings', function(request, callback, notifyCallback) {
   // Set the entry userSettings in the object; this belongs to the user
   var settings = request.user.getSettings();
   settings.userSettings = request.data.userSettings;
   return request.user.setSettings(settings, callback);
 });
 
-wsapi.on('user/set-user-controls', function(request, callback, notifyCallback) {
+wsapi.on<SetUserControlsRequest>('user/set-user-controls', function(request, callback, notifyCallback) {
   if(!canEditUsers(request.user)) {
     request.log.error(`User ${request.user.upn} tried to set controls for user ${request.data.upn}, which they are not allowed to do.`);
     return callback(new Error('Request denied.'));
@@ -34,7 +50,7 @@ wsapi.on('user/get-list', function(request, callback, notifyCallback) {
   return users.getList(callback);
 });
 
-wsapi.on('user/get', function(request, callback, notifyCallback) {
+wsapi.on<UserRequest>('user/get', function(request, callback, notifyCallback) {
   return users.getUser(request.data.upn, (err, result) => {
     if(err)
       return callback(err);
@@ -47,7 +63,7 @@ wsapi.on('user/get', function(request, callback, notifyCallback) {
   });
 });
 
-wsapi.on('user/delete', function(request, callback, notifyCallback) {
+wsapi.on<UserRequest>('user/delete', function(request, callback, notifyCallback) {
   if(!canEditUsers(request.user)) {
     request.log.error(`User ${request.user.upn} tried to delete user ${request.data.upn}, which they are not allowed to do.`);
     return callback(new Error('Request denied.'));
@@ -56,7 +72,7 @@ wsapi.on('user/delete', function(request, callback, notifyCallback) {
   return users.deleteUser(request.data.upn, callback);
 });
 
-wsapi.on('user/create', function(request, callback, notifyCallback) {
+wsapi.on<CreateUserRequest>('user/create', function(request, callback, notifyCallback) {
   if(!canEditUsers(request.user)) {
     request.log.error(`User ${request.user.upn} tried to create user ${request.data.upn}, which they are not allowed to do.`);
     return callback(new Error('Request denied.'));
